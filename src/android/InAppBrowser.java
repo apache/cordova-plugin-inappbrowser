9795b2e9dc540be98c6897a590e707083c570873
Ian Clelland <iclelland@chromium.org>
2014-01-13 15:09:38 -0500

    CB-5756: Add missing import

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -25,6 +25,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
