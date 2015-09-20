b48f7903191fc82d172f8b606a2ad9ce0466b931
Sanket Mehta <sanket.mehta7@gmail.com>
2015-09-20 13:26:39 +0530

    added support for strWindowHeaders in js module

--- www/inappbrowser.js
+++ www/inappbrowser.js
@@ -77,7 +77,7 @@ InAppBrowser.prototype = {
     }
 };
 
-module.exports = function(strUrl, strWindowName, strWindowFeatures, callbacks) {
+module.exports = function(strUrl, strWindowName, strWindowFeatures, strWindowHeaders, callbacks) {
     // Don't catch calls that write to existing frames (e.g. named iframes).
     if (window.frames && window.frames[strWindowName]) {
         var origOpenFunc = modulemapper.getOriginalSymbol(window, 'open');
@@ -97,8 +97,9 @@ module.exports = function(strUrl, strWindowName, strWindowFeatures, callbacks) {
     };
 
     strWindowFeatures = strWindowFeatures || "";
+    strWindowHeaders = strWindowHeaders || "";
 
-    exec(cb, cb, "InAppBrowser", "open", [strUrl, strWindowName, strWindowFeatures]);
+    exec(cb, cb, "InAppBrowser", "open", [strUrl, strWindowName, strWindowFeatures, strWindowHeaders]);
     return iab;
 };
 
