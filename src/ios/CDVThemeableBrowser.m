a149df1ec09e90fbf5592fa602c185d71be166aa
Jörg Runkel <jrunkel@codevise.de>
2015-11-16 00:13:07 +0100

    ios: remove hardcoded density value and add correct param

--- src/ios/CDVThemeableBrowser.m
+++ src/ios/CDVThemeableBrowser.m
@@ -851,7 +851,7 @@
             altDensity = 1.0;
         }
         NSData* data = [NSData dataWithContentsOfFile:path];
-        result = [UIImage imageWithData:data scale:3.0];
+        result = [UIImage imageWithData:data scale:altDensity];
     }
     
     return result;
