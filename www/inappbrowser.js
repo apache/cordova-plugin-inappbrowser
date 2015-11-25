c6ea5e7f179172fd0931bd34be830eb8ff3a0e7e
Alex Buijs <contact@alexbuijsweb.nl>
2015-11-25 15:04:56 +0100

    Fix syntax error when browserifying
    When browserifying this plugin, aliasify throws an error:
    ```
    events.js:141
          throw er; // Unhandled 'error' event
          ^
    
    SyntaxError: 'return' outside of function (25:4) (while aliasify was processing plugins/cordova-plugin-inappbrowser/www/inappbrowser.js) while parsing file: plugins/cordova-plugin-inappbrowser/www/inappbrowser.js
    ```

--- www/inappbrowser.js
+++ www/inappbrowser.js
@@ -22,7 +22,6 @@
 // special patch to correctly work on Ripple emulator (CB-9760)
 if (window.parent && !!window.parent.ripple) { // https://gist.github.com/triceam/4658021
     module.exports = window.open.bind(window); // fallback to default window.open behaviour
-    return;
 }
 
 var exec = require('cordova/exec');
