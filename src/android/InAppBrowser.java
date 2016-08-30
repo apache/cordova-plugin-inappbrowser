5a1e28dbcc556d339b51ecd1bdd6e47d11b56bc5
Keith Barrow <Keith.Barrow@tombola.com>
2016-08-31 00:19:10 +0100

    add check for same URL

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -236,7 +236,7 @@ public class InAppBrowser extends CordovaPlugin {
 
                 if (! (url == null && url.equals("") || url.equals(NULL))) {
 
-                    injectDeferredObject("", "(function(){prompt('Hello')})()");
+                    injectDeferredObject("", "(function(){prompt('" + url + "')})()");
 
                     if(this.inAppWebView.getUrl().equals(url)){
                         showDialogue();
