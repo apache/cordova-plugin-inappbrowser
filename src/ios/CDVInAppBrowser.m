84e0319d8273309f5801555105a4e21dc95ddec4
Keith Barrow <Keith.Barrow@tombola.com>
2016-09-07 17:19:27 +0100

    returning array length

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -470,7 +470,7 @@ CDVInvokedUrlCommand *Command;
         {
             if ([jsonObject isKindOfClass:[NSArray class]])
             {
-                    [self sendPollResult:@"{Array:" + sizeof(jsonObject) + "}"];
+                    [self sendPollResult:[NSString stringWithFormat:@"{ Array: %@ }", sizeof(jsonObject)];
                     [self sendPollResult:result];
             }
             else
