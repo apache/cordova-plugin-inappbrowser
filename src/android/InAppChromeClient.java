f4f9dfb5c071d17379b5d5003dff7149c2f2bf94
Keith Barrow <Keith.Barrow@tombola.com>
2016-09-19 15:43:23 +0100

    minor error in trace code

--- src/android/InAppChromeClient.java
+++ src/android/InAppChromeClient.java
@@ -100,7 +100,7 @@ public class InAppChromeClient extends WebChromeClient {
     @Override
     public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
         // See if the prompt string uses the 'gap-iab' protocol. If so, the remainder should be the id of a callback to execute.
-        LOG.d(LOG_TAG, "response": defaultValue);
+        LOG.d(LOG_TAG, "response: " + defaultValue);
 
         if (defaultValue == null || !defaultValue.startsWith("gap")) {
             return false;
