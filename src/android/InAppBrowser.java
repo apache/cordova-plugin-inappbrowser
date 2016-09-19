4c005ab0b255739fdf525c523974297c4d264ee4
Keith Barrow <Keith.Barrow@tombola.com>
2016-09-19 21:18:05 +0100

    Trying a different incantation

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -77,7 +77,7 @@ import java.util.TimerTask;
 //This looks weird. Java doesn't have the equivalent
 //of delegates, this is the way to do it.
 //default is like internal in c#
-package-private interface NativeScriptResultHandler
+interface NativeScriptResultHandler
 {
     public boolean handle(String scriptResult);
 }
