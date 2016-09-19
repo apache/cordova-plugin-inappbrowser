dc09eff27602e9a0491f5eb1679611356ec6e653
Keith Barrow <Keith.Barrow@tombola.com>
2016-09-19 13:51:01 +0100

    Changed trace reporting interested in ms not s

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -206,7 +206,7 @@ public class InAppBrowser extends CordovaPlugin {
         TimerTask currentTask = new TimerTask(){
             @Override
             public void run() {
-                Log.d(LOG_TAG, "POLL: " + System.currentTimeMillis() / 1000L );
+                Log.d(LOG_TAG, "POLL: " + System.currentTimeMillis());
                 //TODO: ensure poll result returned via correct channel. Not in this function probably.
             }
         };
