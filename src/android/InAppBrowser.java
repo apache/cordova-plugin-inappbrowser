225777d1b1fbc7a913aff73be73b8e001c57c520
Keith Barrow <Keith.Barrow@tombola.com>
2016-10-12 16:36:06 +0100

    Trying to call injected interface

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -250,7 +250,7 @@ public class InAppBrowser extends CordovaPlugin {
                     Log.d(LOG_TAG, "**************************** Bridging");
                     Log.d(LOG_TAG, objectName);
                     inAppWebView.addJavascriptInterface(new JavaScriptBridgeInterface(applicationContext), objectName);
-                    inAppWebView.loadUrl("javascript:" + args.getString(0));
+                    inAppWebView.loadUrl("javascript:" + args.getString(1));
                     Log.d(LOG_TAG, "**************************** Bridging");
                 }
             });
