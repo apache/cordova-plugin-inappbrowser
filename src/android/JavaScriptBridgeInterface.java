871383f09ec3bcfa7b7af57bed8e9538bb051722
Keith Barrow <Keith.Barrow@tombola.com>
2016-10-14 13:09:45 +0100

    Confussed iOS logger with android one...

--- src/android/JavaScriptBridgeInterface.java
+++ src/android/JavaScriptBridgeInterface.java
@@ -22,7 +22,7 @@ public class JavaScriptBridgeInterface {
 
     @JavascriptInterface
     public String respond(String response) {
-        Log.d("Response at " + System.currentTimeMillis());
+        Log.d(LOG_TAG, "Response at " + System.currentTimeMillis());
         if(response.equals("[]")){
             return response;
         }
