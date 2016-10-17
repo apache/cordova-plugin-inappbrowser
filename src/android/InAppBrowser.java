71e8b1afc5d25442a3532ff20ceba544cb46ae90
Keith Barrow <Keith.Barrow@tombola.com>
2016-10-17 11:19:03 +0100

    Better logging

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -898,7 +898,7 @@ public class InAppBrowser extends CordovaPlugin {
             } else {
                 // Assume that everything is HTTP at this point, because if we don't specify,
                 // it really should be.  Complain loudly about this!!!
-                LOG.e(LOG_TAG, "Possible Uncaught/Unknown URI");
+                LOG.e(LOG_TAG, "Possible Uncaught/Unknown URI" + url);
                 newloc = "http://" + url;
             }
 
