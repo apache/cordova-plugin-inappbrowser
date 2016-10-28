1b4859c17520fc3637e0854c260539c7a7858c78
Philippe Morier <thorgal.hayne@outlook.com>
2016-10-28 15:21:08 +0100

    fix(close button): Set correct content description
     This closes #196

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -688,7 +688,7 @@ public class InAppBrowser extends CordovaPlugin {
                 RelativeLayout.LayoutParams closeLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
                 closeLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
                 close.setLayoutParams(closeLayoutParams);
-                forward.setContentDescription("Close Button");
+                close.setContentDescription("Close Button");
                 close.setId(Integer.valueOf(5));
                 int closeResId = activityRes.getIdentifier("ic_action_remove", "drawable", cordova.getActivity().getPackageName());
                 Drawable closeIcon = activityRes.getDrawable(closeResId);
