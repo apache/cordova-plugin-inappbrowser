e7e1b18fda883abeab51aa2545db62048a5c0533
stevemeetswest <hsmcdonald@gmail.com>
2016-12-16 12:38:54 -0800

    Set LOCATION = null

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -76,7 +76,7 @@ public class InAppBrowser extends CordovaPlugin {
     private static final String SELF = "_self";
     private static final String SYSTEM = "_system";
     private static final String EXIT_EVENT = "exit";
-    private static final String LOCATION = "location";
+    private static final String LOCATION = null;
     private static final String ZOOM = "zoom";
     private static final String HIDDEN = "hidden";
     private static final String LOAD_START_EVENT = "loadstart";
