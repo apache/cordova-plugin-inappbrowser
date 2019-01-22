aff73c2542e12c02d816f7aa12c30ca82a58205a
SalehAlhjji <alhjji.s@gmail.com>
2019-01-22 12:14:58 +0300

    Update InAppBrowser.java
    allowing custom protocol

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1356,7 +1356,7 @@ public class InAppBrowser extends CordovaPlugin {
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
             super.onPageStarted(view, url, favicon);
             String newloc = "";
-            if (url.startsWith("http:") || url.startsWith("https:") || url.startsWith("file:")) {
+            if (url.startsWith("http:") || url.startsWith("https:") || url.startsWith("app:")|| url.startsWith("file:")) {
                 newloc = url;
             }
             else
