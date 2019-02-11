apache/cordova-plugin-inappbrowser

commit 9f4b72982abf50ddc54a34bd3748b6a3aad3be57

GH-417: Handle non-default target attribute values (e.g. target=on links in WKWebView implementation on iOS (#418)

src/ios/CDVWKInAppBrowser.m
@@ -573,7 +573,14 @@ static CDVWKInAppBrowser* instance = nil;
     }
     
     if(shouldStart){
-        decisionHandler(WKNavigationActionPolicyAllow);
+        // Fix GH-417: Handle non-default target attribute
+        // Based on https://stackoverflow.com/a/25853806/777265
+        if (!navigationAction.targetFrame.isMainFrame){
+            [theWebView loadRequest:navigationAction.request];
+            decisionHandler(WKNavigationActionPolicyCancel);
+        }else{
+            decisionHandler(WKNavigationActionPolicyAllow);
+        }
     }else{
         decisionHandler(WKNavigationActionPolicyCancel);
     }

commit bdabcb6f5635147d4bdd4f2f4fd69e5afd8b69e9

(iOS) Fix iframes in iOS/WKWebView which were broken by #418. Fixes #424.

src/ios/CDVWKInAppBrowser.m
@@ -573,9 +573,9 @@ static CDVWKInAppBrowser* instance = nil;
     }
     
     if(shouldStart){
-        // Fix GH-417: Handle non-default target attribute
-        // Based on https://stackoverflow.com/a/25853806/777265
-        if (!navigationAction.targetFrame.isMainFrame){
+        // Fix GH-417 & GH-424: Handle non-default target attribute
+        // Based on https://stackoverflow.com/a/25713070/777265
+        if (!navigationAction.targetFrame){
             [theWebView loadRequest:navigationAction.request];
             decisionHandler(WKNavigationActionPolicyCancel);
         }else{