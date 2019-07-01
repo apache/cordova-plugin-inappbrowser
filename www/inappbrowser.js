a1b6999812ca43cb318d7d1d874a19081df8c1af
regevbr <regevbr@gmail.com>
2019-07-01 17:58:04 +0300

    stack overflow when trying to open an iframe #388

--- www/inappbrowser.js
+++ www/inappbrowser.js
@@ -101,7 +101,7 @@
     module.exports = function (strUrl, strWindowName, strWindowFeatures, callbacks) {
         // Don't catch calls that write to existing frames (e.g. named iframes).
         if (window.frames && window.frames[strWindowName]) {
-            var origOpenFunc = modulemapper.getOriginalSymbol(window, 'window.open');
+            var origOpenFunc = modulemapper.getOriginalSymbol(window, 'open');
             return origOpenFunc.apply(window, arguments);
         }
 
