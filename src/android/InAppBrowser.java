aa4094c2fc0106464d01a862f0cabd87b12b2c8e
Ciarán Ó hAoláin <ciaran@cohaolain.ie>
2019-07-23 16:13:20 +0100

    (Android) Remove FLAG_FULLSCREEN
    This causes issues on screens with a notch (the space either side is left blank, the statusbar becomes hidden, which is unwanted behaviour.

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -790,7 +790,6 @@ public class InAppBrowser extends CordovaPlugin {
                 dialog = new InAppBrowserDialog(cordova.getActivity(), android.R.style.Theme_NoTitleBar);
                 dialog.getWindow().getAttributes().windowAnimations = android.R.style.Animation_Dialog;
                 dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
                 dialog.setCancelable(true);
                 dialog.setInAppBroswer(getInAppBrowser());
 
