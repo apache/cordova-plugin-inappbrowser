852467ee759246a4c85da568715c35495e695ba2
Kamil Burek <kamil.burek@collabco.co.uk>
2019-08-01 13:57:41 +0100

    fix #514

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,7 +6,7 @@
 // Copyright (c) Microsoft Open Technologies Inc
 // Licensed under the MIT license.
 // TypeScript Version: 2.3
-type channel = "loadstart" | "loadstop" | "loaderror" | "exit" | "message";
+type channel = "loadstart" | "loadstop" | "loaderror" | "exit" | "message" | string;
 
 interface Window {
     /**
