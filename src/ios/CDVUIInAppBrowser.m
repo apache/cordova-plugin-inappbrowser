b269051f30c72a82afac3f85e586c2b1f90db809
Maik Hummel <maik@neohelden.com>
2019-11-27 17:25:59 +0100

    feat: Add compile-time decision for disabling UIWebView

--- src/ios/CDVUIInAppBrowser.m
+++ src/ios/CDVUIInAppBrowser.m
@@ -17,6 +17,8 @@
  under the License.
  */
 
+#if !WK_WEB_VIEW_ONLY
+
 #import "CDVUIInAppBrowser.h"
 #import <Cordova/CDVPluginResult.h>
 #import <Cordova/CDVUserAgentUtil.h>
@@ -1126,4 +1128,4 @@ static CDVUIInAppBrowser* instance = nil;
 
 @end
 
-
+#endif
