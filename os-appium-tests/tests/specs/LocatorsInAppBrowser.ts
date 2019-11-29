237d8fb1ce7342cb719da4bfcd6bfa04126c6269
Manuel  Garnel <manue.garnel@outsystems.com>
2019-11-29 16:32:08 +0000

    feat: implement PO test on InAppBrowser class using a valid http conncetion
    References RNMT-2847

--- os-appium-tests/tests/specs/LocatorsInAppBrowser.ts
+++ os-appium-tests/tests/specs/LocatorsInAppBrowser.ts
@@ -8,7 +8,7 @@ export const LOCATORS = {
 export  const WEBPAGE_LOCATORS = {
 
     //Arrange
-    YEAH_CLOSE_BUTTON: $("//android.widget.TextView[@content-desc='Close Button']"),
-    MESSAGE_IN_WEBPAGE: $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View[1]/android.view.View[3]'),
-//const containingMessage = $("=Theatrical-Trailer");
+    YEAH_CLOSE_BUTTON: "//android.widget.TextView[@content-desc='Close Button']",
+    MESSAGE_IN_WEBPAGE: '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View[1]/android.view.View[3]',
+    LOCATORCINEMA : '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View[1]/android.view.View[3]/android.view.View/android.view.View[2]'
 };
--- os-appium-tests/tests/specs/inAppBrowser/inAppBrowser.spec.ts
+++ os-appium-tests/tests/specs/inAppBrowser/inAppBrowser.spec.ts
@@ -5,7 +5,7 @@ import PermissionAlert from '../../helpers/PermissionAlert';
 import {url} from "inspector";
 import {DEFAULT_TIMEOUT} from "../../constants";
 import {TIMEOUT} from "dns";
-import {LOCATORS} from "../LocatorsInAppBrowser";
+import {LOCATORS, WEBPAGE_LOCATORS} from "../LocatorsInAppBrowser";
 import * as assert from "assert";
 import * as Assert from "assert";
 
@@ -55,17 +55,26 @@ describe('[TestSuite, Description("Add an URL and open it with right behaviour")
 
     });
 
+    beforeEach(() => {
+            // Wait for webview to load
+            Context.waitForWebViewContextLoaded();
+
+            // Switch the context to WEBVIEW
+            Context.switchToContext(Context.CONTEXT_REF.WEBVIEW);
+
+            // Wait for Home Screen
+            waitForScreen(InAppBrowserScreen.SCREENTITLES.HOME_SCREEN);
+        }
+    );
+
 
     it('[Test, Description("Open valid url  https with locator:  LOCATORS.HTTPS_VALID_URL ),  Priority="P0"]', () => {
 
-        //Arrange
-        const yeahCloseButton = $("//android.widget.TextView[@content-desc='Close Button']");
-        const messageInPage = $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View[1]/android.view.View[3]');
-        //const containingMessage = $("=Theatrical-Trailer");
+        const result = $(WEBPAGE_LOCATORS.MESSAGE_IN_WEBPAGE);
         const expectedResult: string = 'Joker (2019)';
+
         const urlConnection = InAppBrowserScreen.GetURLConnectionWithLocators(LOCATORS.HTTPS_VALID_URL);
         urlConnection.waitForDisplayed(DEFAULT_TIMEOUT);
-
         urlConnection.click();
         const openInAppBrowserButton = InAppBrowserScreen.getSelectInAppBrowserButton();
         openInAppBrowserButton.waitForDisplayed(DEFAULT_TIMEOUT);
@@ -74,29 +83,33 @@ describe('[TestSuite, Description("Add an URL and open it with right behaviour")
         Context.switchToContext(nativeAppContext);
         Context.waitForWebsiteLoaded();
 
-        //  console.log("message joker:  " + messageInPage.getText());
-        //https://medium.com/@AyaAkl/working-with-appium-and-web-views-446400218264
-
-        expect(messageInPage.getText()).toEqual(expectedResult);
-
-        console.log(messageInPage.getText());
-
+        expect(result.getText()).toEqual(expectedResult);
     });
 
-    /* it('[Test, Description("Open valid url http  with locator: HTTP_VALID_URL,  Priority="P0"]', () => {
+    it('[Test, Description("Open valid url http  with locator: HTTP_VALID_URL,  Priority="P0"]', () => {
 
-         //Arrange
-         backToHomeScreen();
+        console.log("\n Começou teste dois \n ");
+        const expectedResult: string = 'Cinemas';
+        const result = $(WEBPAGE_LOCATORS.LOCATORCINEMA);
+        const locatorCinema = $("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View[1]/android.view.View[3]/android.view.View/android.view.View[2]");
 
-         //Act
-         const urlConnection = InAppBrowserScreen.GetURLConnectionWithLocators(LOCATORS.HTTP_VALID_URL);
-         urlConnection.waitForDisplayed();
-         urlConnection.click();
+        const urlConnection = InAppBrowserScreen.GetURLConnectionWithLocators(LOCATORS.HTTP_VALID_URL);
 
-         //Assert
-         //expect(successMessageText).toEqual((locatorUrlAndExpectedResults.get(locators[1])).toUpperCase());
-     });*/
+        console.log("\n Passou depois dourl connection? \n " + urlConnection);
+        urlConnection.waitForDisplayed();
+        urlConnection.click();
+        const openInAppBrowserButton = InAppBrowserScreen.getSelectInAppBrowserButton();
+        openInAppBrowserButton.waitForDisplayed(DEFAULT_TIMEOUT);
+        openInAppBrowserButton.click();
+        let nativeAppContext = browser.getContexts()[1];
 
+        Context.switchToContext(nativeAppContext);
+
+     //
+        //  expect( locatorCinema.getText()).toEqual(expectedResult);
+       expect( result.getText()).toEqual(expectedResult);
+
+    });
     afterEach(() => {
         //Do test teardown here
     });
