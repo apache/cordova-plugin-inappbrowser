7c2810e885e6269fdafb84d754786016ce2d1f82
Tim Brust <github@timbrust.de>
2020-07-02 15:41:21 +0000

    refactor(eslint): use cordova-eslint /w fix (#747)

--- src/browser/InAppBrowserProxy.js
+++ src/browser/InAppBrowserProxy.js
@@ -17,43 +17,37 @@
  * specific language governing permissions and limitations
  * under the License.
  *
-*/
+ */
 
 var modulemapper = require('cordova/modulemapper');
 
-var browserWrap,
-    popup,
-    navigationButtonsDiv,
-    navigationButtonsDivInner,
-    backButton,
-    forwardButton,
-    closeButton;
+var browserWrap, popup, navigationButtonsDiv, navigationButtonsDivInner, backButton, forwardButton, closeButton;
 
 function attachNavigationEvents (element, callback) {
     var onError = function () {
         try {
-            callback({ type: 'loaderror', url: this.contentWindow.location.href }, {keepCallback: true}); // eslint-disable-line standard/no-callback-literal
+            callback({ type: 'loaderror', url: this.contentWindow.location.href }, { keepCallback: true }); // eslint-disable-line standard/no-callback-literal
         } catch (err) {
             // blocked by CORS :\
-            callback({ type: 'loaderror', url: null }, {keepCallback: true}); // eslint-disable-line standard/no-callback-literal
+            callback({ type: 'loaderror', url: null }, { keepCallback: true }); // eslint-disable-line standard/no-callback-literal
         }
     };
 
     element.addEventListener('pageshow', function () {
         try {
-            callback({ type: 'loadstart', url: this.contentWindow.location.href }, {keepCallback: true}); // eslint-disable-line standard/no-callback-literal
+            callback({ type: 'loadstart', url: this.contentWindow.location.href }, { keepCallback: true }); // eslint-disable-line standard/no-callback-literal
         } catch (err) {
             // blocked by CORS :\
-            callback({ type: 'loadstart', url: null }, {keepCallback: true}); // eslint-disable-line standard/no-callback-literal
+            callback({ type: 'loadstart', url: null }, { keepCallback: true }); // eslint-disable-line standard/no-callback-literal
         }
     });
 
     element.addEventListener('load', function () {
         try {
-            callback({ type: 'loadstop', url: this.contentWindow.location.href }, {keepCallback: true}); // eslint-disable-line standard/no-callback-literal
+            callback({ type: 'loadstop', url: this.contentWindow.location.href }, { keepCallback: true }); // eslint-disable-line standard/no-callback-literal
         } catch (err) {
             // blocked by CORS :\
-            callback({ type: 'loadstop', url: null }, {keepCallback: true}); // eslint-disable-line standard/no-callback-literal
+            callback({ type: 'loadstop', url: null }, { keepCallback: true }); // eslint-disable-line standard/no-callback-literal
         }
     });
 
@@ -153,7 +147,9 @@ var IAB = {
 
                 backButton.innerHTML = '←';
                 backButton.addEventListener('click', function (e) {
-                    if (popup.canGoBack) { popup.goBack(); }
+                    if (popup.canGoBack) {
+                        popup.goBack();
+                    }
                 });
 
                 forwardButton = document.createElement('button');
@@ -164,7 +160,9 @@ var IAB = {
 
                 forwardButton.innerHTML = '→';
                 forwardButton.addEventListener('click', function (e) {
-                    if (popup.canGoForward) { popup.goForward(); }
+                    if (popup.canGoForward) {
+                        popup.goForward();
+                    }
                 });
 
                 closeButton = document.createElement('button');
