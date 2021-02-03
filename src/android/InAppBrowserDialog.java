72a1b0aa69e2bc041581b6aca24fbdb4b6a13cda
Bhaal22 <muller.john@gmail.com>
2021-02-03 10:01:46 +0100

    LP: livepepper apps do not close dialog when back is pressed.

--- src/android/InAppBrowserDialog.java
+++ src/android/InAppBrowserDialog.java
@@ -50,7 +50,8 @@ public class InAppBrowserDialog extends Dialog {
             if (this.inAppBrowser.hardwareBack() && this.inAppBrowser.canGoBack()) {
                 this.inAppBrowser.goBack();
             }  else {
-                this.inAppBrowser.closeDialog();
+                //behavior livepepper does not want to have
+                //this.inAppBrowser.closeDialog();
             }
         }
     }
