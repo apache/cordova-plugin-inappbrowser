d735e136cdf409472833558184764ea47d658c85
joanbat <55381916+joanbat@users.noreply.github.com>
2023-10-27 15:36:16 +0200

    GH-706 android: Allow permissions requests (#968)
    Co-authored-by: joanb-moncasoft <55381916+joanb-moncasoft@users.noreply.github.com>

--- src/android/InAppChromeClient.java
+++ src/android/InAppChromeClient.java
@@ -34,6 +34,7 @@ import android.webkit.WebStorage;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.GeolocationPermissions.Callback;
+import android.webkit.PermissionRequest;
 
 public class InAppChromeClient extends WebChromeClient {
 
@@ -45,6 +46,13 @@ public class InAppChromeClient extends WebChromeClient {
         super();
         this.webView = webView;
     }
+    
+    public void onPermissionRequest(final PermissionRequest request) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            request.grant(request.getResources());
+        }
+    }
+
     /**
      * Handle database quota exceeded notification.
      *
